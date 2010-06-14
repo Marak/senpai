ab24d1f5cb645744eb29fe9bdc9082292fb47d5b
Marak Squires <marak.squires@gmail.com>
2010-06-14 02:19:37 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+exports.checkIn = function(){
+  
+  // load all projects
+  
+  // run some tests maybe
+  
+  // touch the ReadMe file
+  
+  // update with a commit message and timestamp
+  
+  return 'done';
+  
+};
