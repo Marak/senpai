b6d6e9216023a46780644c95cbf9843d5fffa223
Marak Squires <marak.squires@gmail.com>
2010-06-14 23:46:14 -0400

    hrmm

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var exec = require('child_process').exec
 
 var colors = require('./lib/colors');
 
+var gits = [];
+
 // named events and broodmother event dispatcher
 var kohai = new events.EventEmitter();
 
@@ -25,6 +27,11 @@ kohai.addListener('commit', function(project){
   exports.commit(project);
 });
 
+kohai.addListener('complete', function(project){
+  sys.puts('## complete() ##'.grey);
+  exports.complete(project);
+});
+
 //sys.puts(JSON.stringify(projects));
 
 exports.checkIn = function(projects){
@@ -41,41 +48,109 @@ exports.checkIn = function(projects){
     sys.puts('many apologies '.red+'senpai'.yellow+', but the '.red.bold+'projects.json'.green.underline+' file is not valid json. '.red+'master crockford'.yellow+' disapproves.'.red.bold)
   }
 
-  kohai.emit('clone', gitPath);
+
+  for(var project in projects){
+    gits.push(projects[project].git);
+  }
+  gits.reverse();
+  kohai.emit('clone', gits.pop());
   
 };
 
 exports.clone = function(gitPath){
-  
-
     var dir = __dirname;
     var appDir = dir + '/appSpace/';
-    sys.puts('Attempting to clone repo @ ' + gitPath);
-    sys.puts(appDir, gitPath);
+    //sys.puts('Attempting to clone repo @ ' + gitPath);
+    //sys.puts(appDir, gitPath);
     var downloadCommand = 'cd ' + appDir + ' && git clone ' + gitPath;
     // Remark: Using 'exec' here because chaining 'spawn' is a PIMFA
-    gitClone = exec(downloadCommand, function (error, stdout, stderr) {
-      if (error !== null) {
-        sys.puts('Failed to clone repo @ ' + gitPath + ' to ' + appDir + ': ' + error);
+    
+    (function(gitPath){
+      gitClone = exec(downloadCommand, function (error, stdout, stderr) {
+        if (error !== null) {
+          //sys.puts('Failed to clone repo @ ' + gitPath + ' to ' + appDir + ': ' + error);
+        }
+        else{
+        }
+        kohai.emit('touch', gitPath);
+      });
+    })(gitPath)
+};
+
+exports.touch = function(gitPath){
+  var dir = __dirname;
+  var appDir = dir + '/appSpace/';
+  var g = gitPath;
+  
+  (function(gitPath){
+    
+    var x = gitPath.split('/');
+    x = x[x.length-1];
+    x = x.replace('.git','');
+    
+    
+    fs.readFile(appDir + x + '/Readme.md', function (err,data) {
+      if (err !== null) {
+        sys.puts('error '.red + err);
       }
       else{
-        kohai.emit('touch');
+        //sys.puts('file got read', data.toString());
+        (function(gitPath){
+          
+          var x = gitPath.split('/');
+          x = x[x.length-1];
+          x = x.replace('.git','');
+          fs.writeFile(appDir + x + '/Readme.md', data.toString() + ' ', function(err) {
+            if (err !== null) {
+              //sys.puts(JSON.stringify(err));
+            }
+            else{
+            }
+            sys.puts("got touched!");
+            kohai.emit('commit', gitPath);
+          });
+        })(gitPath);
       }
     });
+  })(gitPath);
   
 };
 
-exports.touch = function(projectPath){
-  kohai.emit('commit');
-  return projectPath;  
-};
+exports.commit = function(gitPath){
+  
+  var dir = __dirname;
+  var appDir = dir + '/appSpace/';
+  
+  var x = gitPath.split('/');
+  x = x[x.length-1];
+  x = x.replace('.git','');
+  //sys.puts('Attempting to clone repo @ ' + gitPath);
+  sys.puts(appDir, gitPath);
+  var commitCommand = 'cd ' + appDir + x + ' && git commit -a -m "kohai reporting that all is well sanpai. '+x+' has been succesfully updated, tested, and deployed." && git push origin master';
+  sys.puts(commitCommand);
 
-exports.commit = function(projectPath){
-  kohai.emit('complete');
-  return projectPath;  
+  // Remark: Using 'exec' here because chaining 'spawn' is a PIMFA
+  gitCommit = exec(commitCommand, function (error, stdout, stderr) {
+    if (error !== null) {
+      sys.puts('Failed to commit repo @ ' + gitPath + ' to ' + appDir + ': ' + error);
+    }
+    else{
+      
+      if(gits.length){
+         kohai.emit('clone', gits.pop());
+      }
+      else{
+        kohai.emit('complete');
+      }
+      
+    }
+   
+  });
+ 
 };
 
 exports.complete = function(projectPath){
+  sys.puts('complete');
   return 'complete';
 };
 
